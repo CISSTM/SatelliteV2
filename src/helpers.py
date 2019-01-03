2e75a90f58681646951c02738f7948b39a4e4a35
Micha den Heijer <micha@michadenheijer.com>
2019-01-03 13:44:27 +0100

    new way to send values

--- src/helpers.py
+++ src/helpers.py
@@ -33,29 +33,42 @@ def to_send(topic, value):
     global times_saved
     global current_file_output
     times_send += 1
+
+    # Round numbers for when to send
     if isinstance(value, float):
         value = int(value)
+    
     total = {
         topic: value,
         "s": times_send
     }
 
+    ## Starting message
+    str_to_send = "11"
+    ## Topic number
+    str_to_send += str(ord(topic[0])-31)
+    ## Add the value
+    str_to_send += str(format(value, '04d'))
+    ## Add closing message
+    str_to_send += "22"
+
+    ## Add to current save
     current_file_output.append(total)
 
+    ## If it has send a 100 times save the file
     if (times_send % 100 == 0):
+        ## Open or create file to save to
         with open(basepath + '/../data/data' + str(times_saved) +'.json', 'w+') as file_backup:
+            ##Save to a file
             json.dump(current_file_output, file_backup)
+            ## Clear to save
             current_file_output = []
-        
+        ## +1 for time saved so that it saves to a new file
         times_saved += 1
-
-    output = json.dumps(total)
-    str_to_send = ""
-    for i, c in enumerate(output):
-        str_to_send += str(ord(c)-31)
-        str_to_send += "00"
-    to_send = math.sqrt(int(str_to_send))
-    print (str_to_send)
+    
+    ## Convert the string to int to send
+    to_send = int(str_to_send)
+    print (total)
     return
 
 def send(code):
