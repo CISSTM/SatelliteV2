ed755358b1e94c49df7bb11003bff78ad8a7d5fe
Micha den Heijer <micha@michadenheijer.com>
2019-01-27 16:25:44 +0000

    Added sending the gyro functions, sending all data, making sure data is able to send

--- src/helpers.py
+++ src/helpers.py
@@ -3,18 +3,22 @@ import json
 import argparse
 import logging
 
+
 import board
 import digitalio
 import busio
 import time
 import temperature_driver
+import gyro_driver
+
+
 from collections import OrderedDict
 from os import path
 
 basepath = path.dirname(__file__)
 i2c = busio.I2C(board.SCL, board.SDA)
 bme280 = temperature_driver.Adafruit_BME280_I2C(i2c)
-
+bno055 =  gyro_driver.BNO055(i2c)
 
 times_send =  0
 times_saved = 0
@@ -28,8 +32,38 @@ def get_press():
     press = bme280.pressure
     return press
 
+def get_acc():
+    acc = bno055.linear_acceleration[2]
+    return acc
+
+def get_x_angle():
+    angle = bno055.euler[0]
+    return angle
+
+def get_y_angle():
+    angle = bno055.euler[1]
+    return angle
+
+def get_z_angle():
+    angle = bno055.euler[2]
+    return angle
+
+def get_mag_x():
+    mag = bno055.magnetometer[0]
+    return mag
+
+def get_mag_y():
+    mag = bno055.magnetometer[1]
+    return mag
+
+def get_gravity():
+    gravity = bno055.gravity[2]
+    return gravity
+
 def get_alt(P0, P, T):
     h = (((P0/P)**(1/5.257)-1)*(T+273.15))/(0.0065)
+    if (h<0):
+        h = abs(h) + 9000
     return h;
 
 def to_send(topic, value):
@@ -51,6 +85,12 @@ def to_send(topic, value):
     str_to_send = "11"
     ## Topic number
     str_to_send += str(ord(topic[0])-31)
+    if (value < 0):
+        value = abs(value) + 9000
+        value = int(value)
+    while value > 9999:
+        value = value/10
+        value = int(value)
     ## Add the value
     str_to_send += str(format(value, '04d'))
     ## Add closing message
--- src/main.py
+++ src/main.py
@@ -10,6 +10,13 @@ def get_data(sc):
     to_send("temperature", temp)
     to_send("pressure", press)
     to_send("altitude", alt)
+    to_send("baccelleration", get_acc())
+    to_send("x_angle", get_x_angle())
+    to_send("y_angle", get_y_angle())
+    to_send("z_angle", get_z_angle())
+    to_send("ux_mag", get_mag_x())
+    to_send("vy_mag", get_mag_y())
+    to_send("gravity", get_gravity())
     s.enter(0.2, 1, get_data, (sc,))
 
 s.enter(0.2, 1, get_data, (s,))
