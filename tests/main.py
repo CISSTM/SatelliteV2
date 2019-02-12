3ff1a758bdccd2f8907cdf17b15312f87953e1d8
Micha den Heijer <micha@michadenheijer.com>
2019-02-12 17:04:11 +0100

    removed old tests

--- tests/main.py
+++
@@ -1,8 +0,0 @@
-from helpers import *
-import sys
-print("Current python version: "+str(sys.version_info))
-
-get_alt(1013.25, get_press(), get_temp())
-
-for i in range (0, 200):
-    to_send("s", 200)
